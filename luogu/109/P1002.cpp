10526912e21f11f4162757e3823ed529a76ca71f
mbchen <2161936486@qq.com>
2026-04-10 11:41:39 +0800

    update (2026-04-10)

--- luogu/109/P1002.cpp
+++ luogu/109/P1002.cpp
@@ -4,42 +4,80 @@ using namespace std;
 //TLE
 int result = 0;
 int xb, yb, xm, ym;
-bool judge(int x, int y) {
-    if((x == xm-2 && y == ym-1) || (x == xm-2 && y == ym+1) ||
-        (x == xm-1 && y == ym-2) || (x == xm-1 && y == ym+2) || 
-        (x == xm+1 && y == ym-2) || (x == xm+1 && y == ym+2) ||
-        (x == xm+2 && y == ym-1) || (x == xm+2 && y == ym+1) ||
-        (x == xm && y == ym)) {
-            return true;
-    }
-    return false;
-}
-void move(int x, int y) {
-    if(judge(x, y)) {
-        return;
-    }
-    if(x > xb || y > yb) {
-        return;
-    } 
-    if(x == xb && y == yb) {
-        result++;
-        return;
-    }
-    //向下
-    move(x+1, y);
-    //向右
-    move(x, y+1);
-}
+// bool judge(int x, int y) {
+//     if((x == xm-2 && y == ym-1) || (x == xm-2 && y == ym+1) ||
+//         (x == xm-1 && y == ym-2) || (x == xm-1 && y == ym+2) || 
+//         (x == xm+1 && y == ym-2) || (x == xm+1 && y == ym+2) ||
+//         (x == xm+2 && y == ym-1) || (x == xm+2 && y == ym+1) ||
+//         (x == xm && y == ym)) {
+//             return true;
+//     }
+//     return false;
+// }
+// void move(int x, int y) {
+//     if(judge(x, y)) {
+//         return;
+//     }
+//     if(x > xb || y > yb) {
+//         return;
+//     } 
+//     if(x == xb && y == yb) {
+//         result++;
+//         return;
+//     }
+//     //向下
+//     move(x+1, y);
+//     //向右
+//     move(x, y+1);
+// }
+
+// void change(int x, int y, vector<vector<long long>>&f) {
+//     if((x == xm-2 && y == ym-1) || (x == xm-2 && y == ym+1) ||
+//         (x == xm-1 && y == ym-2) || (x == xm-1 && y == ym+2) || 
+//         (x == xm+1 && y == ym-2) || (x == xm+1 && y == ym+2) ||
+//         (x == xm+2 && y == ym-1) || (x == xm+2 && y == ym+1) ||
+//         (x == xm && y == ym)) {
+//             f[x][y] = 0;
+//     }
+//     return;
+// }
+int dx[9] = {0, -2, -2, -1, -1, 1, 1, 2, 2};
+int dy[9] = {0, -1, 1, -2, 2, -2, 2, -1, 1};
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> xb >> yb >> xm >> ym;
     // move(0, 0);
-    vector<vector<int>>f(21, vector<int>(21,0));
-    f[0][1] = 1;
-    f[1][0] = 1;
-    for(int i = 0;i)
-    cout << result;
+    vector<vector<long long>>f(21, vector<long long>(21,0));
+    vector<vector<bool>>ban(21, vector<bool>(21,false));
+    //先ban再dp
+    //如果按照之前的写法把第一行和第一列全部赋值为1,这样会导致错误，因为在被
+    //ban的点的右方是不能到达的，如果在dp的过程中再ban只会更新被ban的点，这样第一列被ban的点
+    //的右方仍然为错误值1，不会被更新为0这样就会导致错误
+    for(int i = 0;i < 9;i++) {
+        int x = xm + dx[i];
+        int y = ym + dy[i];
+        if(x >= 0 && x <= xb && y >= 0 && y <= yb) {
+            ban[x][y] = true;
+        }
+    }
+    if(ban[0][0]) {
+        cout << 0;
+        return 0;
+    }
+    f[0][0] = 1;
+    for(int i = 0;i <= xb;i++) {
+        for(int j = 0;j <= yb;j++) {
+            if(ban[i][j]) {
+                f[i][j] = 0;
+                continue;
+            }
+            if(i >= 1) f[i][j] += f[i-1][j];
+            if(j >= 1) f[i][j] += f[i][j-1];
+        }
+    }
+    cout << f[xb][yb];
     return 0;
 }
 
---
+++ luogu/109/tempCodeRunnerFile.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//递归的方法的最后一个样例TLE
+//只有两种操作Push和pop，当pop出来的数字个数等于n的时候停止递归
+// int n, result = 0, num_push, num_pop;
+// void digui(int i) {
+//     //这里的i为操作次数，每个数字都要经过Push和pop，即两次操作
+//     if(i == 2*n) {
+//         result++;
+//         return;
+//     }
+//     //push，操作数队列数字减少
+//     if(num_push) {
+//         num_push--;
+//         digui(i+1);
+//         num_push++;
+//     }
+//     if(num_pop && num_pop > num_push) {
+//         num_pop--;
+//         digui(i+1);
+//         num_pop++;
+//     }
+// }
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cin >> n;
+//     num_push = n, num_pop = n;
+//     digui(0);
+//     cout << result;
+//     return 0;
+// }
+
+//emmm，看了题解，可以使用卡特兰数C_n = (4n-2)*C_(n-1) / (n+1);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<long long>c(n+1, 0);
+    c[1] = 1;
+    for(int i = 2;i <= n;i++) {
+        c[i] = (4 * i - 2) * c[i-1] / i + 1;
+    }
+    cout << c[n];
+    return 0;
+}
